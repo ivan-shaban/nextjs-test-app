0bb51acbfeb88a3a564dd2a4e3ca82fd6a3f802a
ishaban <ishaban.flash@gmail.com>
2021-03-07 13:42:52 +0300

    add ssr for css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import '../styles/global.css'
 import App, { AppProps } from 'next/app'
 import { IntlProvider } from 'react-intl'
 import React from 'react'
-import { ThemeProvider } from '@material-ui/core'
+import {
+    CssBaseline,
+    ThemeProvider,
+} from '@material-ui/core'
 import { Provider } from 'next-auth/client'
+import Head from 'next/head'
 
 import { getLocaleMessages } from '../lib/intl'
 import { theme } from '../components/theme'
@@ -17,19 +21,37 @@ export interface AppExtendedProps {
 export default function MyApp({
     Component, pageProps, messages, locale, defaultLocale,
 }: AppProps & AppExtendedProps) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side')
+
+        if (jssStyles) {
+            console.log('>> remove ssr css', jssStyles.innerHTML);
+            jssStyles.parentElement.removeChild(jssStyles)
+        }
+    }, [])
+
     return (
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        <Provider session={pageProps.session}>
-            <IntlProvider
-                locale={locale}
-                defaultLocale={defaultLocale}
-                messages={messages}
-            >
-                <ThemeProvider theme={theme}>
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </IntlProvider>
-        </Provider>
+        <React.Fragment>
+            <Head>
+                <title>cyber manager app</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+            <Provider session={pageProps.session}>
+                <IntlProvider
+                    locale={locale}
+                    defaultLocale={defaultLocale}
+                    messages={messages}
+                >
+                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline />
+                    <ThemeProvider theme={theme}>
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </IntlProvider>
+            </Provider>
+        </React.Fragment>
     )
 }
 
